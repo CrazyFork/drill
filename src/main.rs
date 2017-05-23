ffdb8b660d135e9170d6422029bc782237d269a4
Ferran Basora <fcsonline@gmail.com>
2017-05-23 16:56:37 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use std::thread;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
+use std::collections::HashMap;
+
+extern crate regex;
+use regex::Regex;
 
 extern crate yaml_rust;
 use yaml_rust::YamlLoader;
@@ -58,26 +62,48 @@ fn main() {
     },
   };
 
-  let base_url = config_doc["base_url"].as_str().unwrap();
-  let port = config_doc["port"].as_i64().unwrap();
+  let base_url = config_doc["base_url"].as_str().unwrap().clone();
 
   println!("Threads {}", n_threads);
   println!("Iterations {}", n_iterations);
   println!("Base URL {}", base_url);
-  println!("Port {}", port);
 
   let benchmark_docs = YamlLoader::load_from_str(benchmark_file.as_str()).unwrap();
   let benchmark_doc = &benchmark_docs[0];
+  let benchmark = benchmark_doc.as_vec().unwrap();
 
-  println!("Request {}", benchmark_doc.as_vec().unwrap().len());
+  println!("Request {}", benchmark.len());
 
   let mut children = vec![];
 
-  for i in 0..n_threads {
+  for _i in 0..n_threads {
+    let base_url_clone = base_url.to_owned();
+    let benchmark_clone = benchmark.clone();
+
     children.push(thread::spawn(move || {
-      for j in 0..n_iterations {
-        // TODO: read benchmark_doc
-        println!("this is thread number {} iteration {}", i, j)
+      for _j in 0..n_iterations {
+        let mut context = HashMap::new();
+        context.insert("Hi", "Hola");
+
+        for benchmark_item in &benchmark_clone {
+
+          let benchmark_item_url = benchmark_item["request"]["url"].as_str().unwrap();
+          let final_url = base_url_clone.to_string() + benchmark_item_url;
+
+          let re = Regex::new(r"\{\{(.*)\}\}").unwrap();
+
+          println!("(*) {}", benchmark_item["name"].as_str().unwrap());
+          println!("(U) {}", benchmark_item_url);
+
+          for cap in re.captures_iter(benchmark_item_url) {
+            println!("Item call? {}", &cap[1] );
+          }
+
+
+          let client = Client::new();
+          let response = client.get(&final_url).send().unwrap();
+          println!("< status code: {}\n", response.status);
+        }
       }
     }));
   }
@@ -86,10 +112,4 @@ fn main() {
     // Wait for the thread to finish. Returns a result.
     let _ = child.join();
   }
-
-  let client = Client::new();
-
-  let response = client.get(base_url).send().unwrap();
-
-  println!("< status code: {}", response.status);
 }
