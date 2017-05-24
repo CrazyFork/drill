1d11a1b1db996aaa64cfa2016ee97f7a4671a6e4
Ferran Basora <fcsonline@gmail.com>
2017-05-24 14:22:57 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ static NTHREADS: i64 = 3;
 static NITERATIONS: i64 = 2;
 
 extern crate hyper;
-use hyper::client::Client;
+use hyper::client::{Client, Response};
 
 fn read_file(filepath: &str) -> String {
   // Create a path to the desired file
@@ -39,6 +39,38 @@ fn read_file(filepath: &str) -> String {
   }
 
   content
+}
+
+fn resolve_interpolations(url:&str, context: &HashMap<&str, &str>, responses: &HashMap<&str, Value>) -> String {
+  let re = Regex::new(r"\{\{ *([a-z\.]+) *\}\}").unwrap();
+  let result = re.replace(url, |caps: &Captures| {
+    let cap_path: Vec<&str> = caps[1].split(".").collect();
+    let cap_root = cap_path.get(0).unwrap();
+
+    println!("- cap path {}", cap_path.len() as i32);
+    println!("- cap root {}", cap_root);
+
+    match context.get(&caps[1]) {
+      Some(value) => value.to_string(),
+      _ => {
+        match responses.get(&caps[1]) {
+          Some(value) => value.to_string(),
+          _ => {
+            println!("WARNING! Unknown '{}' variable!\n", &caps[1]);
+            "".to_string()
+          }
+        }
+      }
+    }
+  });
+
+  result.to_string()
+}
+
+fn send_request(url: &str) -> Response {
+  let client = Client::new();
+
+  client.get(url).send().unwrap()
 }
 
 fn main() {
@@ -89,56 +121,40 @@ fn main() {
         let mut responses:HashMap<&str, Value> = HashMap::new();
 
         let mut context = HashMap::new();
-        context.insert("item", "1");
+        // context.insert("item", "1");
         context.insert("foo.body.id", "2");
 
         for benchmark_item in &benchmark_clone {
 
           let benchmark_item_url = benchmark_item["request"]["url"].as_str().unwrap();
 
-          let re = Regex::new(r"\{\{ *([a-z\.]+) *\}\}").unwrap();
-
           println!("- {}", benchmark_item["name"].as_str().unwrap());
 
-          let result = re.replace(benchmark_item_url, |caps: &Captures| {
-            let cap_path: Vec<&str> = caps[1].split(".").collect();
-            let cap_root = cap_path.get(0).unwrap();
+          let with_items = benchmark_item["with_items"].as_vec();
+          if with_items.is_some() {
+            println!("- Items: {:?}", with_items.unwrap());
+          }
 
-            println!("- cap path {}", cap_path.len() as i32);
-            println!("- cap root {}", cap_root);
-
-            match context.get(&caps[1]) {
-              Some(value) => value.to_string(),
-              _ => {
-                match responses.get(&caps[1]) {
-                  Some(value) => value.to_string(),
-                  _ => {
-                    println!("WARNING! Unknown '{}' variable!\n", &caps[1]);
-                    "".to_string()
-                  }
-                }
-              }
-            }
-          });
+          let result = resolve_interpolations(benchmark_item_url, &context, &responses);
 
           let final_url = base_url_clone.to_string() + &result;
 
           print!("  {} => {} : ", benchmark_item_url, final_url);
 
-          let mut data = String::new();
-          let client = Client::new();
-          let mut response = client.get(&final_url).send().unwrap();
+          let mut response = send_request(&final_url);
 
           println!("{}\n", response.status);
 
-          if benchmark_item["assign"].as_str().is_some() {
-            let assign_name = benchmark_item["assign"].as_str().unwrap();
+          let assign = benchmark_item["assign"].as_str();
+
+          if assign.is_some() {
+            let mut data = String::new();
 
             response.read_to_string(&mut data).unwrap();
 
-            let v: Value = serde_json::from_str(&data).unwrap();
+            let value: Value = serde_json::from_str(&data).unwrap();
 
-            responses.insert(assign_name, v);
+            responses.insert(assign.unwrap(), value);
           }
         }
       }
